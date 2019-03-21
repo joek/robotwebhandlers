c621d62f1486f405ea6ff4e83c1bd3f23b46f5f9
Johannes Engelke <johannes.engelke@sap.com>
2019-03-21 09:06:39 +0100

    Adjusted events

--- ws/events.go
+++ ws/events.go
@@ -2,8 +2,10 @@ package ws
 
 // BotCommand command input for robot
 type BotCommand struct {
-	Motor *Motor `json:"motor,omitempty"`
-	Event string `json:"command,omitempty"`
+	Motor          *Motor `json:"motor,omitempty"`
+	ShooterCommand string `json:"shooterCommand,omitempty"`
+	Shooter        string `json:"shooter,omitempty"`
+	Event          string `json:"command,omitempty"`
 }
 
 // Motor command
